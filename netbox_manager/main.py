5c0d037eae7f3c11fb5159e00379cd33f666f342
Jan Horstmann <horstmann@osism.tech>
2025-03-04 10:07:49 +0100

    Validate configuration and provide defaults
    Validate configuration options and provide defaults where possible.
    Do not load device or module types if their path is unset.
    Skip load of device or module types if their path does not exist.
    
    Signed-off-by: Jan Horstmann <horstmann@osism.tech>

--- netbox_manager/main.py
+++ netbox_manager/main.py
@@ -14,7 +14,7 @@ from typing_extensions import Annotated
 import warnings
 
 import ansible_runner
-from dynaconf import Dynaconf
+from dynaconf import Dynaconf, Validator, ValidationError
 from jinja2 import Template
 from loguru import logger
 import pynetbox
@@ -39,9 +39,37 @@ settings = Dynaconf(
     load_dotenv=True,
 )
 
-assert type(settings.DEVICETYPE_LIBRARY) is str
-assert type(settings.TOKEN) is str
-assert type(settings.URL) is str
+# Note: Lazy validate configuration
+settings.validators.register(
+    Validator("DEVICETYPE_LIBRARY", is_type_of=str)
+    | Validator("DEVICETYPE_LIBRARY", is_type_of=None, default=None),
+    Validator("MODULETYPE_LIBRARY", is_type_of=str)
+    | Validator("MODULETYPE_LIBRARY", is_type_of=None, default=None),
+    Validator("RESOURCES", is_type_of=str)
+    | Validator("RESOURCES", is_type_of=None, default=None),
+    Validator("TOKEN", is_type_of=str),
+    Validator("URL", is_type_of=str),
+    Validator("IGNORE_SSL_ERRORS", is_type_of=bool)
+    | Validator(
+        "IGNORE_SSL_ERRORS",
+        is_type_of=str,
+        cast=lambda v: v.lower() in ["true", "yes"],
+        default=False,
+    ),
+    Validator("VERBOSE", is_type_of=bool)
+    | Validator(
+        "VERBOSE",
+        is_type_of=str,
+        cast=lambda v: v.lower() in ["true", "yes"],
+        default=False,
+    ),
+)
+
+try:
+    settings.validators.validate_all()
+except ValidationError as e:
+    logger.error(f"Error validating configuration: {e.details}")
+    raise typer.Exit()
 
 nb = pynetbox.api(settings.URL, token=settings.TOKEN)
 
@@ -207,38 +235,58 @@ def run(
                 cancel_callback=lambda: None,
             )
 
-    if not skipdtl or not skipmtl:
+    if (
+        settings.DEVICETYPE_LIBRARY
+        and not skipdtl
+        or settings.MODULETYPE_LIBRARY
+        and not skipmtl
+    ):
         dtl_netbox = NetBox(settings)
 
     # manage devicetype library
-    if not skipdtl:
+    if settings.DEVICETYPE_LIBRARY and not skipdtl:
         logger.info("Manage devicetypes")
 
         dtl_repo = Repo(settings.DEVICETYPE_LIBRARY)
 
-        files, vendors = dtl_repo.get_devices()
-        device_types = dtl_repo.parse_files(files)
+        try:
+            files, vendors = dtl_repo.get_devices()
+            device_types = dtl_repo.parse_files(files)
 
-        dtl_netbox.create_manufacturers(vendors)
-        dtl_netbox.create_device_types(device_types)
+            dtl_netbox.create_manufacturers(vendors)
+            dtl_netbox.create_device_types(device_types)
+        except FileNotFoundError:
+            logger.error(
+                f"Could not load device types in {settings.DEVICETYPE_LIBRARY}"
+            )
 
-    if not skipmtl:
+    if settings.MODULETYPE_LIBRARY and not skipmtl:
         logger.info("Manage moduletypes")
 
         dtl_repo = Repo(settings.MODULETYPE_LIBRARY)
 
-        files, vendors = dtl_repo.get_devices()
-        module_types = dtl_repo.parse_files(files)
+        try:
+            files, vendors = dtl_repo.get_devices()
+            module_types = dtl_repo.parse_files(files)
 
-        dtl_netbox.create_manufacturers(vendors)
-        dtl_netbox.create_module_types(module_types)
+            dtl_netbox.create_manufacturers(vendors)
+            dtl_netbox.create_module_types(module_types)
+        except FileNotFoundError:
+            logger.error(
+                f"Could not load module types in {settings.MODULETYPE_LIBRARY}"
+            )
 
     if not skipres:
         logger.info("Manage resources")
 
         files = []
         for extension in ["yml", "yaml"]:
-            files.extend(glob.glob(os.path.join(settings.RESOURCES, f"*.{extension}")))
+            try:
+                files.extend(
+                    glob.glob(os.path.join(settings.RESOURCES, f"*.{extension}"))
+                )
+            except FileNotFoundError:
+                logger.error(f"Could not load resources in {settings.RESOURCES}")
 
         files.sort(key=get_leading_number)
         files_grouped = []
